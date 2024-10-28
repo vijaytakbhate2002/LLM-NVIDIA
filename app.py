7d1cd6adb48e2e9db0efd1715b36f3ffd1d75d4b
Vijay Takbhate <vijaytakbhate20@gmail.com>
2024-10-28 13:16:47 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from openai import OpenAI
+import streamlit as st
+import re
+
+st.header("GPT - Powred with (NVIDIA/llama-3.1)", divider='rainbow')
+
+client = OpenAI(
+  base_url = "https://integrate.api.nvidia.com/v1",
+  api_key = "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"
+)
+
+user_input = st.text_area("Ask me anything...")
+
+if len(user_input) > 1:
+    try:
+        completion = client.chat.completions.create(
+        model="nvidia/llama-3.1-nemotron-70b-instruct",
+        messages=[{"role":"user","content":user_input}],
+        temperature=0.5,
+        top_p=1,
+        max_tokens=1024,
+        stream=True
+        )
+
+        output = """"""
+
+        for chunk in completion:
+            if chunk.choices[0].delta.content is not None:
+                output += " " + chunk.choices[0].delta.content
+
+        def clean_text(text):
+            text = re.sub(r'\s+', ' ', text).strip()
+            text = re.sub(r'[^a-zA-Z0-9.,!?\'\"\s]', '', text)
+            return text
+        
+        st.markdown(clean_text(output))
+    except:
+        st.error("API Time out")
+    
